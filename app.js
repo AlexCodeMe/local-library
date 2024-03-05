2ca7398756eff429d3c2403446197c9057c6301b
alex moyer <chloegome@icloud.com>
2024-03-05 07:26:33 -0500

    trying to deploy

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://chloegome:chloegome@cluster0.oxzonmz.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const dev_db_url = "mongodb+srv://chloegome:chloegome@cluster0.oxzonmz.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 
 main().catch((err) => console.log(err));
 async function main() {
